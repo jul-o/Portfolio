eb35aac4ee70fae959978bca845ff5b61b65cae1
jul-o <jules.westersang@gmail.com>
2022-06-16 15:25:45 +0200

    added icons for links, and made resume clickable (#30)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,10 @@ import Contact from 'components/Contact';
 import Experiences from 'components/Experiences';
 import Home from 'components/Home';
 import Navbar from 'components/Navbar';
-import Head from 'next/head';
 
 const Page = () => {
 	return (
 		<>
-			<Head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossOrigin="true"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-					rel="stylesheet"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
-
 			<Navbar />
 			<div id="root">
 				<Home />
