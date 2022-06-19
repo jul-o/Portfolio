b9c100789002c597b7cd68b47f6611e42f08bb25
Jules Sang <jules.westersang@gmail.com>
2022-06-19 19:04:07 +0200

    implemented dark mode (#41)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
+import { ThemeProvider } from 'next-themes';
 import 'styles/globals.scss';
 import 'styles/tags.scss';
 
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<ThemeProvider>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Experiences from 'components/Experiences';
 import Home from 'components/Home';
 import Navbar from 'components/Navbar';
 import UpButton from 'components/UpButton';
+import DarkModeButton from 'components/DarkModeButton';
 
 const Page = () => {
 	return (
@@ -14,6 +15,7 @@ const Page = () => {
 				<Experiences />
 				<Contact />
 			</div>
+			<DarkModeButton />
 			<UpButton />
 		</>
 	);
