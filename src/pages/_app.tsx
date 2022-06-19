ef74e4e03177c667c8961a79eebf9c2261adeee0
Jules Sang <jules.westersang@gmail.com>
2022-06-19 22:50:34 +0200

    logging the tech stack + link to source in the console (#43)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,15 @@ import 'styles/tags.scss';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
+	console.log(
+		'Bonjour ami développeur !\n' +
+			'Vous vous demandez sans doute quelles technologies sont utilisées pour ce site ? Voici la tech stack :\n' +
+			'\t- Next.js + Typescript\n' +
+			"\t- SCSS (je préfère éviter de surcharger mes balises avec des styles inline, d'où l'absence de tailwind, bootstrap ou autre)\n" +
+			'\t- Firebase Hosting et Firebase Functions\n' +
+			'Vous pourrez trouver le code source ici : https://github.com/jul-o/Portfolio'
+	);
+
 	return (
 		<ThemeProvider>
 			<Component {...pageProps} />
