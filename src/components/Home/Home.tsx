8ed9af3be300404a4010d1c5493c4e1666694312
Julo <jules.sang@grenoble-inp.org>
2022-06-20 16:26:37 +0200

    animated home on load

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,15 @@
-// styles import
 import styles from './Home.module.scss';
 
 const Home = () => {
 	return (
 		<div id="home" className={styles.root}>
 			<div className={styles.title}>
-				<h2 className={styles.iam}>Bonjour, moi c'est</h2>
-				<h2 className={styles.name}>Jules Sang.</h2>
+				<h2 className={styles.iam}>
+					<span>Bonjour, moi c'est Jules</span>
+					<span className={styles.wave}>
+						<img src="/wave.svg" width="48" height="48" />
+					</span>
+				</h2>
 				<h3 className={styles.job}>Développeur frontend.</h3>
 			</div>
 		</div>
--- src/next.config.js
+++ src/next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
 	distDir: '../.next',
+	// webpack(config) {
+	// 	config.module.rules.push({
+	// 		test: /\.svg$/i,
+	// 		issuer: /\.[jt]sx?$/,
+	// 		use: [
+	// 			{
+	// 				loader: '@svgr/webpack',
+	// 				options: {
+	// 					icon: true,
+	// 				},
+	// 			},
+	// 		],
+	// 	});
+
+	// 	return config;
+	// },
 };
