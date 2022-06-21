6506d80baf5a73b2f3c266eadef56ade7bf70fab
Julo <jules.sang@grenoble-inp.org>
2022-06-21 17:14:27 +0200

    waving animation restarts on hover

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import classNames from 'utils/classNames';
 import styles from './Home.module.scss';
 
 const Home = () => {
@@ -5,10 +6,24 @@ const Home = () => {
 		<div id="home" className={styles.root}>
 			<div className={styles.title}>
 				<h2 className={styles.iam}>
-					<span>Bonjour, moi c'est Jules</span>
-					<span className={styles.wave}>
+					<div>Bonjour, moi c'est Jules</div>
+					<div
+						className={classNames(styles.wave, styles.animated)}
+						onAnimationEnd={(e) => {
+							(e.target as HTMLDivElement).classList.remove(styles.animated);
+						}}
+						onMouseEnter={(e) => {
+							(e.currentTarget as HTMLDivElement).classList.add(
+								styles.animated
+							);
+						}}
+						onTouchStart={(e) => {
+							(e.currentTarget as HTMLDivElement).classList.add(
+								styles.animated
+							);
+						}}>
 						<img src="/wave.svg" width="48" height="48" />
-					</span>
+					</div>
 				</h2>
 				<h3 className={styles.job}>Développeur frontend.</h3>
 			</div>
