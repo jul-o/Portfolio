d1c39a006170ac9177b5291f1bdffd15841ef699
Jules Sang <jules.westersang@gmail.com>
2022-06-22 19:28:55 +0200

    created ImageWithSpinner component (#49)

--- src/components/About/Portrait/Portrait.tsx
+++ src/components/About/Portrait/Portrait.tsx
@@ -1,18 +1,21 @@
-import Image from 'next/image';
+import Image from 'components/ImageWithSpinner';
 import { useState } from 'react';
 import styles from './Portrait.module.scss';
-import myPic from 'styles/assets/me.png';
 import classNames from 'utils/classNames';
+import { BounceLoader } from 'react-spinners';
+import { css } from '@emotion/react';
+import myPic from 'styles/assets/me.png';
 
 const Portrait = () => {
 	const [hover, setHover] = useState(false);
+	const [isImageLoaded, setIsImageLoaded] = useState(false);
 
 	return (
 		<figure
 			tabIndex={0}
 			className={classNames(styles.imageWrapper, hover && styles.hover)}>
 			<div className={styles.imageBorder} />
-			<Image src={myPic} width={256} height={256} className={styles.image} />
+			<Image src={myPic} className={styles.image} />
 		</figure>
 	);
 };
