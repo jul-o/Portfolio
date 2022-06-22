614d6e3cb6e35eda479d8e47a2c7991c6797e84e
Jules Sang <jules.westersang@gmail.com>
2022-06-22 13:42:48 +0200

    satisfying gradient + gradient animation (#47)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,10 @@ import DarkModeButton from 'components/DarkModeButton';
 const Page = () => {
 	return (
 		<>
+			<div id="background">
+				<div id="background-1" />
+			</div>
+
 			<Navbar />
 			<div id="root">
 				<Home />
