a62d2c25734cd04435809570e308c726d53af720
Julo <jules.sang@grenoble-inp.org>
2022-06-25 21:26:29 +0200

    Stopped using homemade Image component, onLoad might be the problem

--- src/components/About/Modals/WindsurfingModal/WindsurfingModal.tsx
+++ src/components/About/Modals/WindsurfingModal/WindsurfingModal.tsx
@@ -1,6 +1,6 @@
 import Modal from 'components/Modal';
 import styles from '../Modals.module.scss';
-import Image from 'components/Image';
+import Image from 'next/image';
 import text from 'data_frontend/windsurfing.json';
 import image from 'styles/assets/windsurfing.jpg';
 
@@ -14,11 +14,9 @@ const WindsurfingModal = ({ onClose }: Props) => {
 			<div className={styles.modal_body}>
 				<span className={styles.modal_image}>
 					<Image
-						width={1430}
-						height={1079}
 						src={image}
 						alt="Planche à voile à Mèze avec l'EVUG"
-						blur={true}
+						placeholder="blur"
 					/>
 				</span>
 				<div className={styles.text}>
