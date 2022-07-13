bbdc840a9e7984f7e78ef01c179b0f4314b5a389
Jules Sang <jules.westersang@gmail.com>
2022-07-13 21:55:25 +0200

    Replaced blur with a spinner

--- src/components/sections/About/Modals/WindsurfingModal/WindsurfingModal.tsx
+++ src/components/sections/About/Modals/WindsurfingModal/WindsurfingModal.tsx
@@ -1,6 +1,6 @@
 import Modal from 'components/reusable/Modal';
+import Image from 'components/reusable/Image';
 import styles from '../Modals.module.scss';
-import Image from 'next/image';
 import text from 'data_frontend/windsurfing.json';
 import image from 'styles/assets/windsurfing.jpg';
 
