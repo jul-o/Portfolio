e5a31d96c15bd8c03629c54ed0b8c305daf95634
Jules Sang <jules.westersang@gmail.com>
2022-07-20 18:23:20 +0200

    fixed #89, changed light primary and secondary colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,18 +21,22 @@ const Page = () => {
 	return (
 		<>
 			<div id="root">
-				<motion.div
-					animate={backgroundAnimation}
-					initial={{ opacity: 0 }}
-					id="background"
-				/>
+				{/* div that takes the whole content height. Useful for drag boundaries */}
+				<div id="content">
+					<motion.div
+						animate={backgroundAnimation}
+						initial={{ opacity: 0 }}
+						id="background"
+					/>
 
-				<Navbar />
-				<Home className="section" />
-				<About className="section" />
-				<Experiences className="section" />
-				<Contact className="section" />
+					<Navbar />
+					<Home className="section" />
+					<About className="section" />
+					<Experiences className="section" />
+					<Contact className="section" />
+				</div>
 			</div>
+
 			<DarkModeButton />
 			<UpButton />
 		</>
