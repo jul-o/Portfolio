786434bf79e0899bbc8592f8cd52bb69e1b564d1
Jules Sang <jules.sang@grenoble-inp.org>
2023-10-06 18:02:43 +0200

    Fix square portrait

--- src/components/sections/About/Portrait/Portrait.tsx
+++ src/components/sections/About/Portrait/Portrait.tsx
@@ -1,27 +1,29 @@
-import Image from 'components/reusable/Image';
-import myPic from 'styles/assets/me.png';
-import classNames from 'utils/classNames';
-import styles from './Portrait.module.scss';
+import Image from "components/reusable/Image";
+import myPic from "styles/assets/me.png";
+import classNames from "utils/classNames";
+import styles from "./Portrait.module.scss";
 
 interface Props {
-	className?: string;
+  className?: string;
 }
 
 const Portrait = ({ className }: Props) => {
-	return (
-		<figure tabIndex={0} className={classNames(className, styles.imageWrapper)}>
-			<div className={styles.imageBorder} />
-			<Image
-				src={myPic}
-				alt="Photo de moi, beau gosse, chauve, en hoodie et avec des lunettes"
-				className={styles.image}
-				rootClassName={styles.imageRoot}
-				width={256}
-				height={256}
-				placeholder="empty"
-			/>
-		</figure>
-	);
+  return (
+    <figure tabIndex={0} className={classNames(className, styles.imageWrapper)}>
+      <div className={styles.imageBorder} />
+      <div className={styles.imageContent}>
+        <Image
+          src={myPic}
+          alt="Photo de moi, beau gosse, chauve, en hoodie et avec des lunettes"
+          className={styles.image}
+          rootClassName={styles.imageRoot}
+          width={256}
+          height={256}
+          placeholder="empty"
+        />
+      </div>
+    </figure>
+  );
 };
 
 export default Portrait;
