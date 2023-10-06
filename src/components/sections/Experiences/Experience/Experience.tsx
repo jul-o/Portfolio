d7a91c5084f1ece387636f4efc5970269ed63717
Jules Sang <jules.sang@grenoble-inp.org>
2023-10-06 10:56:17 +0200

    Add Hopia

--- src/components/sections/Experiences/Experience/Experience.tsx
+++ src/components/sections/Experiences/Experience/Experience.tsx
@@ -1,90 +1,94 @@
-import { motion, Variants } from 'framer-motion';
-import { ReactElement, useEffect, useState } from 'react';
-import classNames from 'utils/classNames';
-import styles from './Experience.module.scss';
-import { rootVariants } from 'utils/slideInVariants';
+import { motion, Variants } from "framer-motion";
+import { ReactElement, useEffect, useState } from "react";
+import classNames from "utils/classNames";
+import styles from "./Experience.module.scss";
+import { rootVariants } from "utils/slideInVariants";
 
 interface Props {
-	isShown?: boolean;
-	isExperiencesInViewport?: boolean;
-	title: string;
-	dates: string;
-	technologies: Array<{
-		name: string;
-		percentage: number;
-		color?: string;
-	}>;
-	tldr?: Array<string>;
-	body?: ReactElement;
-	variants?: Variants;
+  isShown?: boolean;
+  isExperiencesInViewport?: boolean;
+  title: string;
+  dates: string;
+  technologies: Array<{
+    name: string;
+    percentage: number;
+    iconFiletype?: string;
+    color?: string;
+  }>;
+  tldr?: Array<string>;
+  body?: ReactElement;
+  variants?: Variants;
 }
 
 const Experience = ({
-	title,
-	dates,
-	technologies,
-	tldr,
-	body,
-	isShown = true,
-	isExperiencesInViewport = true,
-	variants,
+  title,
+  dates,
+  technologies,
+  tldr,
+  body,
+  isShown = true,
+  isExperiencesInViewport = true,
+  variants,
 }: Props) => {
-	const experienceVariants: Variants = {
-		notInViewport: ({ isExperiencesInViewport, isShown }) => ({
-			...rootVariants.notInViewport,
-			// disappear instantly when switching experiences
-			...(isExperiencesInViewport ||
-				(!isShown && {
-					display: 'none',
-					transition: {},
-				})),
-		}),
-		inViewport: {
-			...rootVariants.inViewport,
-			// reappear before the slide in, in case of switching experiences
-			display: 'inherit',
-			transition: {
-				...rootVariants.inViewport.transition,
-				staggerChildren: 0.03,
-			},
-		},
-	};
+  const experienceVariants: Variants = {
+    notInViewport: ({ isExperiencesInViewport, isShown }) => ({
+      ...rootVariants.notInViewport,
+      // disappear instantly when switching experiences
+      ...(isExperiencesInViewport ||
+        (!isShown && {
+          display: "none",
+          transition: {},
+        })),
+    }),
+    inViewport: {
+      ...rootVariants.inViewport,
+      // reappear before the slide in, in case of switching experiences
+      display: "inherit",
+      transition: {
+        ...rootVariants.inViewport.transition,
+        staggerChildren: 0.03,
+      },
+    },
+  };
 
-	return (
-		<motion.div
-			animate={
-				isShown && isExperiencesInViewport ? 'inViewport' : 'notInViewport'
-			}
-			variants={experienceVariants}
-			custom={{ isExperiencesInViewport }}
-			initial={'notInViewport'}>
-			<div className={styles.titles}>
-				<motion.h2 variants={variants}>{title}</motion.h2>
-				<motion.p variants={variants} className={styles.dates}>
-					{dates}
-				</motion.p>
-			</div>
-			<div className={styles.technologies}>
-				{technologies.map(({ name, percentage, color }) => (
-					<motion.span variants={variants} key={name}>
-						<img
-							src={`/icons/${color}-logo.svg`}
-							alt=""
-							className={color && styles[color]}
-						/>
-						<span className={color && styles[color]}>{name}</span>
-					</motion.span>
-				))}
-			</div>
-			<div className={styles.tldr}>
-				<motion.h3 variants={variants}>TL;DR :</motion.h3>
-				<motion.ul variants={variants}>
-					{tldr && tldr.map((item) => <li key={item}>{item}</li>)}
-				</motion.ul>
-			</div>
-			<div className={styles.body}>{body}</div>
-		</motion.div>
-	);
+  return (
+    <motion.div
+      animate={
+        isShown && isExperiencesInViewport ? "inViewport" : "notInViewport"
+      }
+      variants={experienceVariants}
+      custom={{ isExperiencesInViewport }}
+      initial={"notInViewport"}
+    >
+      <div className={styles.titles}>
+        <motion.h2 variants={variants}>{title}</motion.h2>
+        <motion.p variants={variants} className={styles.dates}>
+          {dates}
+        </motion.p>
+      </div>
+      <div className={styles.technologies}>
+        {technologies.map(({ name, color, iconFiletype }) => (
+          <motion.span variants={variants} key={name}>
+            <img
+              src={`/icons/${color}-logo.${
+                iconFiletype !== undefined ? iconFiletype : "svg"
+              }`}
+              alt=""
+              className={color && styles[color]}
+            />
+            <span className={color && styles[color]}>{name}</span>
+          </motion.span>
+        ))}
+      </div>
+      <div className={styles.tldr}>
+        <motion.h3 variants={variants}>TL;DR :</motion.h3>
+        <motion.ul variants={variants}>
+          {tldr && tldr.map((item) => <li key={item}>{item}</li>)}
+        </motion.ul>
+      </div>
+      <div className={styles.body}>{body}</div>
+    </motion.div>
+  );
 };
 
 export default Experience;
--- src/components/sections/Experiences/Experiences.tsx
+++ src/components/sections/Experiences/Experiences.tsx
@@ -1,95 +1,98 @@
-import Experience from 'components/sections/Experiences/Experience';
-import experiences from 'data_frontend/experiences.json';
-import React, { useEffect, useState } from 'react';
-import classNames from 'utils/classNames';
-import useViewportObserver from 'utils/hooks/useViewportObserver';
-import styles from './Experiences.module.scss';
-import { useLocalStorage } from 'react-use';
-import { rootVariants, childrenVariants } from 'utils/slideInVariants';
-import { motion, Variants } from 'framer-motion';
+import Experience from "components/sections/Experiences/Experience";
+import experiences from "data_frontend/experiences.json";
+import React, { useEffect, useState } from "react";
+import classNames from "utils/classNames";
+import useViewportObserver from "utils/hooks/useViewportObserver";
+import styles from "./Experiences.module.scss";
+import { useLocalStorage } from "react-use";
+import { rootVariants, childrenVariants } from "utils/slideInVariants";
+import { motion, Variants } from "framer-motion";
 
 const Experiences = ({ className }) => {
-	const [storageIndex, updateStorageIndex] = useLocalStorage(
-		'selected-index',
-		'0'
-	);
-	const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const [storageIndex, updateStorageIndex] = useLocalStorage(
+    "selected-index",
+    "0"
+  );
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
-	useEffect(() => {
-		setSelectedIndex(parseInt(storageIndex));
-	}, []);
+  useEffect(() => {
+    setSelectedIndex(parseInt(storageIndex));
+  }, []);
 
-	useEffect(() => {
-		updateStorageIndex(selectedIndex.toString());
-		console.log(storageIndex);
-	}, [selectedIndex]);
+  useEffect(() => {
+    updateStorageIndex(selectedIndex.toString());
+  }, [selectedIndex]);
 
-	const [isInViewport, setIsInViewport] = useState(false);
-	const rootRef = React.createRef<HTMLDivElement>();
-	useViewportObserver(setIsInViewport, rootRef);
+  const [isInViewport, setIsInViewport] = useState(false);
+  const rootRef = React.createRef<HTMLDivElement>();
+  useViewportObserver(setIsInViewport, rootRef);
 
-	return (
-		<div
-			id="experiences"
-			className={classNames(styles.root, className)}
-			ref={rootRef}>
-			<motion.div
-				variants={rootVariants}
-				animate={isInViewport ? 'inViewport' : 'notInViewport'}
-				initial={'notInViewport'}>
-				<motion.h1 variants={childrenVariants}>
-					Mes expériences de développeur
-				</motion.h1>
-				<div className={styles.experiences_wrapper}>
-					<motion.div
-						variants={childrenVariants}
-						className={styles.experience_selector}
-						id="experience-selector">
-						<motion.div
-							variants={childrenVariants}
-							className={styles[`selected_margin--${selectedIndex}`]}
-						/>
+  return (
+    <div
+      id="experiences"
+      className={classNames(styles.root, className)}
+      ref={rootRef}
+    >
+      <motion.div
+        variants={rootVariants}
+        animate={isInViewport ? "inViewport" : "notInViewport"}
+        initial={"notInViewport"}
+      >
+        <motion.h1 variants={childrenVariants}>
+          Mes expériences de développeur
+        </motion.h1>
+        <div className={styles.experiences_wrapper}>
+          <motion.div
+            variants={childrenVariants}
+            className={styles.experience_selector}
+            id="experience-selector"
+          >
+            <motion.div
+              variants={childrenVariants}
+              className={styles[`selected_margin--${selectedIndex}`]}
+            />
 
-						{experiences.map((experience, index) => (
-							<motion.button
-								variants={childrenVariants}
-								className={classNames(
-									styles.button,
-									index === selectedIndex && styles.selected
-								)}
-								key={experience.title}
-								onClick={(e) => {
-									setSelectedIndex(index);
-								}}>
-								{experience.company}
-							</motion.button>
-						))}
-					</motion.div>
-					<div id="display-experience">
-						{experiences.map((experience, index) => (
-							<Experience
-								variants={childrenVariants}
-								{...experience}
-								isShown={index === selectedIndex}
-								isExperiencesInViewport={isInViewport}
-								body={
-									<>
-										{experience.body.map((item, index) => (
-											// TODO: do not give DOM, only the text. let Experience handle the dom
-											<motion.p variants={childrenVariants} key={index}>
-												{item}
-											</motion.p>
-										))}
-									</>
-								}
-								key={experience.title}
-							/>
-						))}
-					</div>
-				</div>
-			</motion.div>
-		</div>
-	);
+            {experiences.map((experience, index) => (
+              <motion.button
+                variants={childrenVariants}
+                className={classNames(
+                  styles.button,
+                  index === selectedIndex && styles.selected
+                )}
+                key={experience.title}
+                onClick={() => {
+                  setSelectedIndex(index);
+                }}
+              >
+                {experience.company}
+              </motion.button>
+            ))}
+          </motion.div>
+          <div id="display-experience">
+            {experiences.map((experience, index) => (
+              <Experience
+                variants={childrenVariants}
+                {...experience}
+                isShown={index === selectedIndex}
+                isExperiencesInViewport={isInViewport}
+                body={
+                  <>
+                    {experience.body.map((item, index) => (
+                      // TODO: do not give DOM, only the text. let Experience handle the dom
+                      <motion.p variants={childrenVariants} key={index}>
+                        {item}
+                      </motion.p>
+                    ))}
+                  </>
+                }
+                key={experience.title}
+              />
+            ))}
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Experiences;
