4ee4f92642627820edd7e5d58fa85eb51a26f788
Jules Sang <jules.sang@grenoble-inp.org>
2025-05-29 15:58:04 +0200

    Change description : Firebase -> Vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,12 +11,12 @@ import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   console.log(
     "Bonjour ami développeur !\n" +
-      "Vous vous demandez sans doute quelles technologies sont utilisées pour ce site ? Voici la stack :\n" +
-      "\t- Next.js + Typescript\n" +
-      "\t- SCSS (J'ai décidé de ne pas surcharger mes balises avec des styles inline, d'où l'absence de Tailwind, Emotion ou autre)\n" +
-      "\t- Framer Motion\n" +
-      "\t- Firebase Hosting et Firebase Functions\n" +
-      "Vous pourrez trouver le code source ici : https://github.com/jul-o/Portfolio"
+    "Vous vous demandez sans doute quelles technologies sont utilisées pour ce site ? Voici la stack :\n" +
+    "\t- Next.js + Typescript\n" +
+    "\t- SCSS (J'ai décidé de ne pas surcharger mes balises avec des styles inline, d'où l'absence de Tailwind, Emotion ou autre)\n" +
+    "\t- Framer Motion\n" +
+    "\t- Vercel\n" +
+    "Vous pourrez trouver le code source ici : https://github.com/jul-o/Portfolio"
   );
 
   return (
