9450f6e0a55671e0a252294bc15878bd0f5067f7
Jules Sang <jules.sang@grenoble-inp.org>
2025-05-29 15:22:44 +0200

    Reduce transition duration for modals

--- src/components/reusable/Modal/Modal.tsx
+++ src/components/reusable/Modal/Modal.tsx
@@ -7,76 +7,76 @@ import classNames from 'utils/classNames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface Props {
-	onClose: () => void;
-	header?: ReactNode;
-	children: ReactNode;
-	footer?: ReactNode;
-	style?: React.CSSProperties;
-	className?: string;
+  onClose: () => void;
+  header?: ReactNode;
+  children: ReactNode;
+  footer?: ReactNode;
+  style?: React.CSSProperties;
+  className?: string;
 }
 
 const Modal = ({
-	onClose,
-	header,
-	children,
-	footer,
-	style,
-	className,
+  onClose,
+  header,
+  children,
+  footer,
+  style,
+  className,
 }: Props) => {
-	useEffect(() => {
-		// const html = document.getElementsByTagName('html');
-		const html = document.querySelector('html');
+  useEffect(() => {
+    // const html = document.getElementsByTagName('html');
+    const html = document.querySelector('html');
 
-		const previousOverflowY = html.style.overflowY;
-		html.style.overflowY = 'hidden';
+    const previousOverflowY = html.style.overflowY;
+    html.style.overflowY = 'hidden';
 
-		return () => {
-			html.style.overflowY = previousOverflowY;
-		};
-	}, []);
+    return () => {
+      html.style.overflowY = previousOverflowY;
+    };
+  }, []);
 
-	const rootAnimation = {
-		transform: ['scale(0, 0.01)', 'scale(1, 0.01)', 'scale(1, 1)'],
-	};
-	const rootDuration = 0.4;
+  const rootAnimation = {
+    transform: ['scale(0, 0.01)', 'scale(1, 0.01)', 'scale(1, 1)'],
+  };
+  const rootDuration = 0.2;
 
-	const modalAnimation = {
-		opacity: ['0', '1'],
-		scale: ['0.8', '1'],
-	};
-	const modalDuration = 0.5;
+  const modalAnimation = {
+    opacity: ['0', '1'],
+    scale: ['0.8', '1'],
+  };
+  const modalDuration = 0.2;
 
-	return createPortal(
-		<AnimatePresence>
-			<motion.div
-				animate={rootAnimation}
-				transition={{ duration: rootDuration }}
-				className={classNames(className, styles.root)}
-				onClick={onClose}
-				style={style}>
-				<motion.div
-					animate={modalAnimation}
-					transition={{ duration: modalDuration, delay: rootDuration }}
-					className={styles.modal}
-					onClick={(e) => {
-						// prevent root's onClick from closing the modal
-						e.stopPropagation();
-					}}>
-					{header && (
-						<div className={styles.header}>
-							<h1>{header}</h1>
-							<button className={styles.button_close} onClick={onClose}>
-								<FontAwesomeIcon icon={faXmark} />
-							</button>
-						</div>
-					)}
-					<div className={styles.body}>{children}</div>
-					{footer && <div className={styles.footer}>{footer}</div>}
-				</motion.div>
-			</motion.div>
-		</AnimatePresence>,
-		document.body
-	);
+  return createPortal(
+    <AnimatePresence>
+      <motion.div
+        animate={rootAnimation}
+        transition={{ duration: rootDuration }}
+        className={classNames(className, styles.root)}
+        onClick={onClose}
+        style={style}>
+        <motion.div
+          animate={modalAnimation}
+          transition={{ duration: modalDuration, delay: rootDuration }}
+          className={styles.modal}
+          onClick={(e) => {
+            // prevent root's onClick from closing the modal
+            e.stopPropagation();
+          }}>
+          {header && (
+            <div className={styles.header}>
+              <h1>{header}</h1>
+              <button className={styles.button_close} onClick={onClose}>
+                <FontAwesomeIcon icon={faXmark} />
+              </button>
+            </div>
+          )}
+          <div className={styles.body}>{children}</div>
+          {footer && <div className={styles.footer}>{footer}</div>}
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>,
+    document.body
+  );
 };
 
 export default Modal;
